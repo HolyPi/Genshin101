f387c179d7f1363d2661b8215ca7f04dde5103b7
HolyPi <joanelly2018@gmail.com>
2021-01-08 15:21:06 -0500

    Added elements decoration

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Body.css'
+import elem from '../../static/images/element.png'
 
 
 export default function Header() {
     return (
         
         <div className="quote-section">
+            <div className="deco"><img src={elem}></img></div>
             <h1><span>For everything</span>
             <span className="ital">Genshin Impact</span></h1>
         </div>
