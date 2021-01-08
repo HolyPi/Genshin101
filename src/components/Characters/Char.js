c14dab2976b17fcdeaf2df32f9d4b2087bffcd6d
HolyPi <joanelly2018@gmail.com>
2021-01-08 18:53:47 -0500

    Added bg of circle + information

--- src/components/Characters/Char.js
+++ src/components/Characters/Char.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Char.css'
 import holder from '../../static/images/Diluc.webp'
+import bg from '../../static/images/mon.jpg'
+import five from '../../static/images/5.webp'
+import four from'../../static/images/4.webp'
 
 export default function Char() {
     return (
@@ -8,11 +11,17 @@ export default function Char() {
         <div className="side-nav">
         </div>
         <div className="char-container">
+            
             <div className="char-portrait">
                 <img src={holder}></img>
+                <div className="name">Diluc</div>
+                <div className="desc">The tycoon of a winery empire in Mondstadt,
+                unmatched in every possible way.</div>
+               
             </div>
-            <div className="circle"></div>
+            <div className="circle"><img src={bg}></img></div>
         </div>  
+        
         </div>     
     )
 }
