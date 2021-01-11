cd1cce133d8efb64190ea8581c0c1e37799b6547
HolyPi <joanelly2018@gmail.com>
2021-01-10 19:34:26 -0500

    displays character & default character

--- src/components/Characters/Char.js
+++ src/components/Characters/Char.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {render} from 'react-dom';
 import './Char.css'
 import { useState, useEffect } from 'react'
 import holder from '../../static/images/Diluc.webp'
@@ -9,18 +10,42 @@ const axios = require("axios")
 
 export default function Char() {
     const [allChar, setAllChar] = useState([])
+    const [oneChar, setChar] = useState()
+    const [charName, setCharName] = useState("albedo")
     const url = "https://api.genshin.dev/characters/all"
+    function changeChar() {
+
+    }
     useEffect( () => {
         async function fetchData()  {
             const data = await axios.get(url)
             setAllChar(data.data.map((char) => {
-                console.log(char)
-                console.log(char.name.toLowerCase())
-                return {name: char.name, icon: `https://api.genshin.dev/characters/${char.name.toLowerCase()}/icon`}
+                return {name: char.name, rarity: char.rarity, icon: `https://api.genshin.dev/characters/${char.name.toLowerCase()}/icon`}
             }))
         }
         fetchData();
     }, [])
+    const url2 = `https://api.genshin.dev/characters/${charName}`
+    useEffect( () => {
+        async function fetchChar()  { 
+        const data2 = await axios.get(url2)
+        const {name, description, rarity, portrait} = data2.data
+        const charJSX = (<div>
+            <div className="char-container">
+            <div className="char-portrait"> 
+            <img className="port" src={`https://api.genshin.dev/characters/${name.toLowerCase()}/portrait`}></img>
+            <p className="chname">{name}</p>
+            <p className="chdesc">{description}</p>
+            <img className="rarity" src={five}></img>
+            </div>
+            <div className="circle"><img src={bg}></img></div>
+            </div>
+            </div>)
+        setChar(charJSX)   
+        console.log(data2)
+        }
+        fetchChar();
+    }, [charName])
     return (
         <div>
         <div className="side-nav">
@@ -30,25 +55,15 @@ export default function Char() {
                 <div>
                     <ul class="char-name">{char.name} </ul>
                     <img className="char-image" src={char.icon}></img>
-                    
-                </div>
+            
+          </div>
             )
+
         })}
         </div>
         </div>
-        <div className="char-container">
-        <div className="char-portrait"> 
-                <img className="port" src={holder}></img>
-                <p className="chname">Diluc</p>
-                <p className="chdesc">The tycoon of a winery empire in Mondstadt,
-                unmatched in every possible way.</p>
-                <img className="rarity" src={five}></img>
-               
-            </div>
-            <div className="circle"><img src={bg}></img></div>
-        </div>  
-        
-        </div>  
+        {oneChar}
+        </div> 
         
            
     )
