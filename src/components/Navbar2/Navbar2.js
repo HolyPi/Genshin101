064717b46c32e03f2ca95b3f2a069fadb1c8f9ea
HolyPi <joanelly2018@gmail.com>
2021-01-11 17:29:53 -0500

    Small fixes to layout + resized elem icons

--- src/components/Navbar2/Navbar2.js
+++ src/components/Navbar2/Navbar2.js
@@ -9,7 +9,7 @@ export default function Navbar2() {
         <nav>
             <a href="/"><img className="logo2" src={si} alt="logo"></img></a>
             <ul className="nav-links">
-                <li className="litwo"><Link to="/characters">Characters</Link></li>
+                <li className="active-page"><Link to="/characters">Characters</Link></li>
                 <li className="litwo"><Link to="/elements">Elements</Link></li>
                 <li className="litwo"><Link to="/domains">Domains</Link></li>
             </ul>
